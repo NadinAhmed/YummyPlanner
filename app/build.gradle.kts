a64126b487b0118ca67b3147a98dc79f6dab5631
Nadin Ahmed <nadinahmed316@gmail.com>
2026-02-01 19:33:08 +0200

    adding login screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation(libs.lottie)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    implementation (libs.segmentedbutton)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
